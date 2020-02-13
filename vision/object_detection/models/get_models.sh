22d0cb17b82bb8774cf359bbb0cfe389ad2b5085
dxs <glaglasven@live.com>
2020-02-13 20:59:56 +0000

    gitignore and get_models.sh

---
+++ vision/object_detection/models/get_models.sh
@@ -0,0 +1,9 @@
+wget https://pjreddie.com/media/files/yolov3.weights
+
+wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg?raw=true -O ./yolov3.cfg
+
+wget https://github.com/pjreddie/darknet/blob/master/data/coco.names?raw=true -O ./coco.names
+
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3-tiny.cfg
+
+wget https://pjreddie.com/media/files/yolov3-tiny.weights
